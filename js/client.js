48485e0c63e72fb0353b825f57191ddcd67c4cd6
Myka Peligrino <mykapeligrino@gmail.com>
2022-11-03 23:06:09 -0600

    major update

--- js/client.js
+++ js/client.js
@@ -3,31 +3,80 @@ const randomInteger = function(max) {
   return randomInteger;
 }
 
-const fetchData = async function(endpoint) {
-  const response = await fetch(endpoint);
-  // stores json to data to become usable js
-  const data = await response.json();
-  // Use the data
-  //loop this for the assignment
-  const anime = data[randomInteger(data.length)];
+const section = document.querySelector('.data');
 
-  const body = document.querySelector('body');
+const fetchData = async function(endpoint) {
+  try{
+    // error handling
+    const response = await fetch(endpoint);
+    // stores json to data to become usable js
+    const data = await response.json();
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    // Use the data
+    //loop this for the assignment
+    // anime = data[randomInteger(data.length)];
+
+    // const body = document.querySelector('body');
   
-  output = `
-    <article>
-      <h2>${anime.title}</h2>
-      <img src="${anime.image}" alt"Picture of ${anime.title}">
-      <ul>
-        <li>Director: ${anime.director}</li>
-        <li>Producer: ${anime.producer}</li>
-        <li>Date: ${anime.release_date}</li>
-        <li>Description: ${anime.description}</li>
-        <li>Rotten Tomato Score: ${anime.rt_score}</li>
-      </ul>
-    </article>
-    `
-  body.innerHTML = output;
+    // output = `
+    //   <article>
+    //     <h2>${anime.title}</h2>
+    //     <img src="${anime.image}" alt"Picture of ${anime.title}">
+    //     <ul>
+    //       <li>Director: ${anime.director}</li>
+    //       <li>Producer: ${anime.producer}</li>
+    //       <li>Date: ${anime.release_date}</li>
+    //       <li>Description: ${anime.description}</li>
+    //       <li>Rotten Tomato Score: ${anime.rt_score}</li>
+    //    </ul>
+    //   </article>
+    //   `
+    // body.innerHTML = output;
+
+    // accumulator
+    let output = '';
+
+    const handlePost = function(data) {
+      output += `
+       <article>
+         <h2>${data.title}</h2>
+         <img src="${data.image}" alt"Picture of ${data.title}">
+         <ul>
+           <li>Director: ${data.director}</li>
+           <li>Producer: ${data.producer}</li>
+           <li>Date: ${data.release_date}</li>
+           <li>Description: ${data.description}</li>
+           <li>Rotten Tomato Score: ${data.rt_score}</li>
+           <a href="${data.url}">Complete information</a>
+        </ul>
+       </article>
+       `
+    }
+
+    data.forEach(handlePost);
+
+    // prints output
+    section.innerHTML = output;
+
+  } catch (error) {
+    // This code only runs when an exception (error) is thrown
+    console.log('Message: ', error.message);
+    console.log('Name: ', error.name);
+    console.log('Cause: ', error.cause);
+
+    document.querySelector('body').innerHTML = `<h1>There was an error</h1>`
+  }
+  
+
+
+  
 }
 
 // Invoke our function
-fetchData('https://ghibliapi.herokuapp.com/films'); 
+fetchData('https://ghibliapi.herokuapp.com/films');
