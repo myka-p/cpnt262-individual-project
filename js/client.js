17c3dd749a5b09cc1e125f8868db35f1f345b145
Myka Peligrino <mykapeligrino@gmail.com>
2022-11-04 23:22:27 -0600

    major css style update

--- js/client.js
+++ js/client.js
@@ -2,13 +2,13 @@ const section = document.querySelector('.data');
 
 const fetchData = async function(endpoint) {
   try{
-    // error handling
     const response = await fetch(endpoint);
     // stores json to data to become usable js
     const data = await response.json();
 
     console.log(response);
 
+    // error handling
     if (!response.ok) {
       throw new Error(`${response.status}: ${response.statusText}`);
     }
@@ -20,19 +20,20 @@ const fetchData = async function(endpoint) {
       output += `
        <article class="card">
          <h2>${data.title}</h2>
-         <img src="${data.image}" alt"Picture of ${data.title}">
+         <figure><img src="${data.image}" alt"Picture of ${data.title}"></figure>
          <ul>
-           <li>Director: ${data.director}</li>
-           <li>Producer: ${data.producer}</li>
-           <li>Date: ${data.release_date}</li>
-           <li>Description: ${data.description}</li>
-           <li>Rotten Tomato Score: ${data.rt_score}</li>
-           <li><a href="${data.url}">Complete Information</a></li>
-        </ul>
+           <li><strong>Director:</strong> ${data.director}</li>
+           <li><strong>Producer:</strong> ${data.producer}</li>
+           <li><strong>Date:</strong> ${data.release_date}</li>
+           <li><strong>Description:</strong> <br>${data.description}</li>
+           <li><strong>Rotten Tomato Score:</strong> ${data.rt_score}</li>
+          </ul>
+          <a href="${data.url}">Film Index</a>
        </article>
        `
     }
 
+    // loops the array from API
     data.forEach(handlePost);
 
     // prints output
